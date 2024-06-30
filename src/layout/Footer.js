0bf3f0f852848ad1689c83be37dc10b3282a5de6
RyongJaeKwon <gyj343@gmail.com>
2024-07-01 01:30:31 +0900

    Refact: css 코드 리팩토링

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,10 +1,8 @@
 const Footer = () => {
     return (
-        <footer className="bg-white text-black p-4 mt-8">
-            <div className="container mx-auto">
-                <p>&copy; 2024 My Shop All rights reserved</p>
-            </div>
-        </footer>
+        <div className="bg-gray-800 text-white text-xl py-20 text-center">
+            © 2024 My Shop
+        </div>
     )
 }
 
