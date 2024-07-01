d51bdf2179ab3a4a435af8dd3f1cd244a175ba00
RyongJaeKwon <gyj343@gmail.com>
2024-07-01 21:50:43 +0900

    Feat: add 경로 추가

--- src/router/memberRouter.js
+++ src/router/memberRouter.js
@@ -2,12 +2,17 @@ import { Suspense, lazy } from "react";
 
 const Loading = <div>Loading...</div>
 const Login = lazy(() => import("../pages/member/LoginPage"))
+const MemberAdd = lazy(() => import("../pages/member/SignUpPage"))
 
 const memberRouter = () => {
     return [
         {
             path: "login",
             element: <Suspense fallback={Loading}><Login/></Suspense>
+        },
+        {
+            path: "add",
+            element: <Suspense fallback={Loading}><MemberAdd/></Suspense>
         }
     ]
 }
