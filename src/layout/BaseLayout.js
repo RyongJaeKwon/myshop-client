0c777832b6be1ce1ec929e9729d88f39c6b60860
RyongJaeKwon <gyj343@gmail.com>
2024-07-01 21:45:18 +0900

    Feat: useLocation 훅 추가

--- src/layout/BaseLayout.js
+++ src/layout/BaseLayout.js
@@ -1,19 +1,25 @@
+import { useLocation } from "react-router-dom";
 import CategoryNav from "./CategoryNav";
 import Footer from "./Footer";
 import Header from "./Header";
 
 const BaseLayout = ({children}) => {
+    
+    const location = useLocation();
+    const isLoginPage = location.pathname === '/member/login';
+    const isLoginForm = location.pathname === '/member/add';
+
     return (
         <div className="min-h-screen flex justify-center">
             <div className="w-full max-w-screen-xl px-4 md:px-8 lg:px-20">
                 <Header/>
                 <hr className="my-4 border-gray-800"/>
-                <CategoryNav/>
-                <div className="bg-white shadow-sm border border-gray-100 min-h-screen">
+                {!isLoginPage && !isLoginForm ? <CategoryNav/> : <></>}
+                <div className="min-h-screen">
                     {children}
                 </div>
                 
-                <Footer/>
+                {!isLoginPage && !isLoginForm ? <Footer/> : <></>}
             </div>
         </div>
     )
