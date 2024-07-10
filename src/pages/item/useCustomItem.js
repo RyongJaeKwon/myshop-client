8f73c70dd9237bd0f5afc614e7aaceef0db03769
RyongJaeKwon <gyj343@gmail.com>
2024-07-10 16:21:03 +0900

    Fix: Item hook 로직 수정

---
+++ src/pages/item/useCustomItem.js
@@ -0,0 +1,35 @@
+import { createSearchParams, useNavigate, useSearchParams } from "react-router-dom"
+
+const getNum = (param, defaultValue) => {
+    if(!param){
+        return defaultValue
+    }
+    return parseInt(param)
+}
+
+const useCustomItem = (category) => {
+    const navigate = useNavigate()
+    const [queryParams] = useSearchParams()
+    const page = getNum(queryParams.get('page'), 1)
+    const size = getNum(queryParams.get('size'), 6)
+    const queryDefault = createSearchParams({page, size}).toString()
+
+    const moveToList = (param) => {
+        let queryStr = ""
+
+        if(param){
+            const pageNum = getNum(param.page, 1)
+            const sizeNum = getNum(param.size, 6)
+
+            queryStr = createSearchParams({page:pageNum, size:sizeNum}).toString()
+        }else {
+            queryStr = queryDefault
+        }
+
+        navigate({pathname:`/items/${category}`, search:queryStr})
+    }
+
+    return {page, size, moveToList}
+}
+
+export default useCustomItem
