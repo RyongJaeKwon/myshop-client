26cdbfde9603a29974f4f5d2c7118f15593e345d
RyongJaeKwon <gyj343@gmail.com>
2024-07-10 16:24:30 +0900

    Refact: MainPage를 GridLayout 컴포넌트로 모듈화

---
+++ src/pages/item/GridLayout.js
@@ -0,0 +1,9 @@
+const GridLayout = ({children}) => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-10">
+            {children}
+        </div>
+    )
+}
+
+export default GridLayout
