1ef83ce764491222016ce36c5d32c6fdbc3cd27a
RyongJaeKwon <gyj343@gmail.com>
2024-07-10 16:18:19 +0900

    Fix: Link 경로 수정

--- src/layout/CategoryNav.js
+++ src/layout/CategoryNav.js
@@ -21,12 +21,12 @@ const CategoryNav = ({toggleRefresh}) => {
         <nav className="bg-white text-black text-xl">
             <div className="flex justify-between items-center">
                 <Link to={'/'} className="flex-grow text-center hover:bg-gray-300 py-2 px-2 rounded-full">ALL</Link>
-                <Link to={'/items/top/'} className="flex-grow text-center hover:bg-gray-300 py-2 px-2 rounded-full">TOP</Link>
-                <Link to={'/items/pants/'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">BOTTOM</Link>
-                <Link to={'/items/outer/'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">OUTER</Link>
-                <Link to={'/items/cap/'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">HAT</Link>
-                <Link to={'/items/shoes/'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">SHOES</Link>
-                <Link to={'/items/acc/'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">ACC</Link>
+                <Link to={'/items/top'} className="flex-grow text-center hover:bg-gray-300 py-2 px-2 rounded-full">TOP</Link>
+                <Link to={'/items/bottom'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">BOTTOM</Link>
+                <Link to={'/items/outer'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">OUTER</Link>
+                <Link to={'/items/hat'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">HAT</Link>
+                <Link to={'/items/shoes'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">SHOES</Link>
+                <Link to={'/items/acc'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">ACC</Link>
                 {loginState.role === 'ROLE_ADMIN' ? 
                 <button className="flex-grow text-center text-white py-3 rounded-full bg-blue-500" onClick={openModal}>상품등록</button>
                 :
