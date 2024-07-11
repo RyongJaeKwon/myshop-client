cc921417c4f35a38adbf29e41cb058132c58fcac
RyongJaeKwon <gyj343@gmail.com>
2024-07-11 20:12:00 +0900

    Fix: 조회 화면으로 이동 기능 추가

--- src/pages/item/ItemLayout.js
+++ src/pages/item/ItemLayout.js
@@ -1,14 +1,21 @@
 import { API_SERVER_HOST } from "../../api/memberApi"
+import useCustomItem from "./useCustomItem"
 
-const ItemLayout = ({item}) => {
+const ItemLayout = ({item, toggleRefresh}) => {
     const host = API_SERVER_HOST
+    const { moveToRead, page, size } = useCustomItem()
+
+    const handleClickImage = (e) => {
+        moveToRead({category: item.category, id:item.id, page, size, toggleRefresh})
+    }
 
     return (
         <div className="p-4">
             {item.uploadFileNames && item.uploadFileNames.length > 0 && (
-                <img src={`${host}/items/view/${item.uploadFileNames}`} alt="itemimage"
+                <img src={`${host}/items/view/${item.uploadFileNames[0]}`} alt="itemimage"
                     className="w-full h-auto rounded-md object-cover"
-                    style={{ width: "400px", height: "400px"}}/>
+                    style={{ width: "400px", height: "400px"}}
+                    onClick={handleClickImage}/>
             )}
             <div className="flex justify-between mt-1">
                 <div>{item.color}</div>
