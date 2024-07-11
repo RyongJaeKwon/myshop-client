9e5bb24f1e87a3b43f9636512327d868d5b85601
RyongJaeKwon <gyj343@gmail.com>
2024-07-11 20:09:26 +0900

    Fix: 각 카테고리 경로 수정

--- src/layout/CategoryNav.js
+++ src/layout/CategoryNav.js
@@ -1,19 +1,20 @@
 import { useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import ItemAddModal from "../components/item/ItemAddModal";
 import { useState } from "react";
 
 const CategoryNav = ({toggleRefresh}) => {
     const loginState = useSelector(state => state.loginSlice)
-    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false)
     const navigate = useNavigate()
+    const location = useLocation()
 
-    const openModal = () => {
-        setIsModalOpen(true);
+    const openAddModal = () => {
+        setIsAddModalOpen(true);
     };
     
-    const closeModal = () => {
-        setIsModalOpen(false)
+    const closeAddModal = () => {
+        setIsAddModalOpen(false)
         navigate('/')
     }
 
@@ -21,19 +22,19 @@ const CategoryNav = ({toggleRefresh}) => {
         <nav className="bg-white text-black text-xl">
             <div className="flex justify-between items-center">
                 <Link to={'/'} className="flex-grow text-center hover:bg-gray-300 py-2 px-2 rounded-full">ALL</Link>
-                <Link to={'/items/top'} className="flex-grow text-center hover:bg-gray-300 py-2 px-2 rounded-full">TOP</Link>
-                <Link to={'/items/bottom'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">BOTTOM</Link>
-                <Link to={'/items/outer'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">OUTER</Link>
-                <Link to={'/items/hat'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">HAT</Link>
-                <Link to={'/items/shoes'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">SHOES</Link>
-                <Link to={'/items/acc'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">ACC</Link>
-                {loginState.role === 'ROLE_ADMIN' ? 
-                <button className="flex-grow text-center text-white py-3 rounded-full bg-blue-500" onClick={openModal}>상품등록</button>
+                <Link to={'/items/top?page=1&size=6'} className="flex-grow text-center hover:bg-gray-300 py-2 px-2 rounded-full">TOP</Link>
+                <Link to={'/items/bottom?page=1&size=6'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">BOTTOM</Link>
+                <Link to={'/items/outer?page=1&size=6'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">OUTER</Link>
+                <Link to={'/items/hat?page=1&size=6'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">HAT</Link>
+                <Link to={'/items/shoes?page=1&size=6'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">SHOES</Link>
+                <Link to={'/items/acc?page=1&size=6'} className="flex-grow text-center hover:bg-gray-300 py-3 px-2 rounded-full">ACC</Link>
+                {loginState.role === 'ROLE_ADMIN' && location.pathname === '/' ? 
+                <button className="flex-grow text-center text-white py-3 rounded-full bg-blue-500" onClick={openAddModal}>상품등록</button>
                 :
                 <></>
                 }
             </div>
-            {isModalOpen && <ItemAddModal callbackFn={closeModal} toggleRefresh={toggleRefresh} />}
+            {isAddModalOpen && <ItemAddModal closeAddModal={closeAddModal} toggleRefresh={toggleRefresh} />}
         </nav>
     )
 }
