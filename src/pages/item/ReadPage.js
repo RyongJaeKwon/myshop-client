8586c6ade393f3ee66a19288209d299f68afb580
RyongJaeKwon <gyj343@gmail.com>
2024-07-11 20:13:37 +0900

    Feat: ReadPage 컴포넌트 추가

---
+++ src/pages/item/ReadPage.js
@@ -0,0 +1,11 @@
+import ReadComponent from "../../components/item/ReadComponent"
+
+const ReadPage = () => {
+    return (
+        <div className="w-full h-full">
+            <ReadComponent/>
+        </div>
+    )
+}
+
+export default ReadPage
