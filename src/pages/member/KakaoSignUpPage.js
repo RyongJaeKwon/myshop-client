d5912af9e7bf5802593f4217a4a7812532131ddc
RyongJaeKwon <gyj343@gmail.com>
2024-07-18 03:26:08 +0900

    Feat: 카카오 회원가입 페이지 구현

---
+++ src/pages/member/KakaoSignUpPage.js
@@ -0,0 +1,13 @@
+import KakaoSignUpComponent from "../../components/member/KakaoSignUpComponent"
+
+const KakaoSignUpPage = () => {
+    return (
+        <div className="w-full h-full">
+            <div className="flex justify-center items-center">
+                <KakaoSignUpComponent/>
+            </div>
+        </div>
+    )
+}
+
+export default KakaoSignUpPage
