fd48ac26e03b077f2187bce921443fc5b90571e1
RyongJaeKwon <gyj343@gmail.com>
2024-07-18 03:20:39 +0900

    Fix: 카카오 회원가입 경로에 대한 CategoryNav 조건부 렌더링

--- src/layout/BaseLayout.js
+++ src/layout/BaseLayout.js
@@ -6,15 +6,16 @@ import Header from "./Header";
 const BaseLayout = ({children, toggleRefresh}) => {
     
     const location = useLocation();
-    const isLoginPage = location.pathname === '/member/login';
-    const isLoginForm = location.pathname === '/member/signup';
+    const isLoginPage = location.pathname === '/member/login'
+    const isLoginForm = location.pathname === '/member/signup'
+    const isUpdateForm = location.pathname === '/member/kakaoSignup'
 
     return (
         <div className="min-h-screen flex justify-center">
             <div className="w-full max-w-screen-xl px-4 md:px-8 lg:px-20">
                 <Header/>
                 <hr className="my-4 border-gray-800"/>
-                {!isLoginPage && !isLoginForm ? <CategoryNav toggleRefresh={toggleRefresh}/> : <></>}
+                {!isLoginPage && !isLoginForm && !isUpdateForm ? <CategoryNav toggleRefresh={toggleRefresh}/> : <></>}
                 <div className="min-h-screen">
                     {children}
                 </div>
