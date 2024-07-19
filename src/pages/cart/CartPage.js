0e4fde5127af7274baba664aefdd71103b26bb9e
RyongJaeKwon <gyj343@gmail.com>
2024-07-20 05:13:03 +0900

    Feat: 장바구니 관련 화면 페이지 구현

---
+++ src/pages/cart/CartPage.js
@@ -0,0 +1,13 @@
+import CartComponent from "../../components/cart/CartComponent"
+
+const CartPage = () => {
+    return(
+        <div className="w-full h-full">
+            <div className="flex justify-center items-center mt-10">
+                <CartComponent/>
+            </div>
+        </div>
+    )
+}
+
+export default CartPage
---
+++ src/pages/cart/IndexPage.js
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+import BaseLayout from "../../layout/BaseLayout"
+
+const IndexPage = () => {
+    return (
+        <BaseLayout>
+            <div className="flex flex-wrap w-full">
+                <Outlet/>
+            </div>
+        </BaseLayout>
+    )
+}
+
+export default IndexPage
