7979683569638ff3e601127f1d674b763d483bdf
RyongJaeKwon <gyj343@gmail.com>
2024-07-20 17:20:17 +0900

    Fix: 장바구니 확인 버튼 클릭 시 장바구니 페이지로 이동하는 기능 추가

--- src/hooks/useCartHook.js
+++ src/hooks/useCartHook.js
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from "react-redux"
 import { deleteOneAsync, getCartItemsAsync, postChangeCartAsync } from "../slices/cartSlice"
 import { useCallback, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 const useCartHook = () => {
     const cartItemList = useSelector(state => state.cartSlice.cartItemList)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const changeCart = (cartItem) => {
         dispatch(postChangeCartAsync(cartItem))
@@ -18,11 +20,15 @@ const useCartHook = () => {
         dispatch(deleteOneAsync(cartItemId))
     }
 
+    const moveToCart = () => {
+        navigate('/cart/list')
+    }
+
     useEffect(() => {
         refreshCart()
     }, [refreshCart])
 
-    return {changeCart, refreshCart, deleteCartItem, cartItemList: cartItemList}
+    return {changeCart, refreshCart, deleteCartItem, moveToCart, cartItemList: cartItemList}
 
 }
 
