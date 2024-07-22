c9e7ba547438e4de76d1546066bd3a2dd6d3f7cd
RyongJaeKwon <gyj343@gmail.com>
2024-07-22 22:13:22 +0900

    Fix: 카카오 로그인 후처리 로직 수정

--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -2,32 +2,39 @@ import { useEffect } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom"
 import { getAccessToken, getMemberWithAccessToken } from "../../api/kakaoApi"
 import { useDispatch } from "react-redux"
-import { setCookie } from "../../util/cookieManager"
-import { login } from "../../slices/loginSlice"
+import { loginAsync } from "../../slices/loginSlice"
 
 const KakaoRedirectPage = () => {
     const [param] = useSearchParams()
     const auth_code = param.get("code")
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const state = param.get("state")
 
     useEffect(() => {
-        getAccessToken(auth_code).then(accessToken => {
-            console.log("accessToken: ", accessToken)
-
-            getMemberWithAccessToken(accessToken).then(memberInfo => {
+        const performLogin = async () => {
+            try {
+                const accessToken = await getAccessToken(auth_code)
+                console.log("accessToken: ", accessToken)
+        
+                const memberInfo = await getMemberWithAccessToken(accessToken)
                 console.log("memberInfo: ", memberInfo)
-
+                
                 if (memberInfo.id) {
-                    dispatch(login(memberInfo))
-                    navigate('/', {replace:true})
-                    setCookie("member", JSON.stringify(memberInfo), 1)
+                    await dispatch(loginAsync(memberInfo))
+                    const from = state || '/'
+                    navigate(from, { replace: true });
                 } else {
                     navigate('/member/kakaoSignup', {state: {memberInfo}})
                 }
-            })
-        })
-    }, [auth_code, dispatch, navigate])
+            } catch (error) {
+                console.error('Error during login:', error)
+            }
+        }
+
+        performLogin()
+
+    }, [auth_code, dispatch, navigate, state])
 
     return (
         <div>
