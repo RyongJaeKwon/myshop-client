230e3f0ab888efe9efa30f6f6b939722f2ab31c6
RyongJaeKwon <gyj343@gmail.com>
2024-07-22 22:07:44 +0900

    Fix: 카카오 로그인 링크 생성 시 Redirect_url 파라미터 인코딩 추가

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -6,8 +6,9 @@ const redirect_uri = `http://localhost:3000/member/kakao`
 const auth_code_path = `https://kauth.kakao.com/oauth/authorize`
 const access_token_url = `https://kauth.kakao.com/oauth/token`
 
-export const getKakaoLoginLink = () => {
-    const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+export const getKakaoLoginLink = (from) => {
+    const state = encodeURIComponent(from)
+    const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code&state=${state}`;
 
     return kakaoURL
 }
