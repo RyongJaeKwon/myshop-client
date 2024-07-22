9b93f5f009336ecd155a4150f6772522ab5cf305
RyongJaeKwon <gyj343@gmail.com>
2024-07-22 22:11:34 +0900

    Fix: 카카오 로그인 후 Redirect_url를 인자로 받도록 수정

--- src/components/member/KakaoLoginComponent.js
+++ src/components/member/KakaoLoginComponent.js
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 import { getKakaoLoginLink } from '../../api/kakaoApi';
 import kakao from '../../img/kakao_login.png';
 
-const KakaoLoginComponent = () => {
-    const link = getKakaoLoginLink()
+const KakaoLoginComponent = ({from}) => {
+    const link = getKakaoLoginLink(from)
 
     return (
         <div className="flex justify-center mt-2">
