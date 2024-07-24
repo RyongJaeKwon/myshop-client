2bd64f473196c0f410048f59576e5c5a8294db41
RyongJaeKwon <gyj343@gmail.com>
2024-07-24 14:54:20 +0900

    Fix: keyword를 인자로 받아 페이지 이동 시 검색어 유지하도록 수정

--- src/components/common/Pagination.js
+++ src/components/common/Pagination.js
@@ -1,18 +1,18 @@
-const Pagenation = ({serverData, movePage}) => {
+const Pagenation = ({serverData, movePage, keyword}) => {
     return (
         <div className="m-6 flex justify-center">
             {serverData.prev ? <div className="m-2 p-2 w-16 text-center font-bold text-blue-400"
-            onClick={() => movePage({page: serverData.prevPage})}>이전</div> : <></>}
+            onClick={() => movePage({page: serverData.prevPage, keyword})}>이전</div> : <></>}
 
             {serverData.pageList.map(pageNum => <div key={pageNum} className={`m-2 p-2 w-12 text-center rounded shadow-md text-white
             ${serverData.current === pageNum ? "bg-gray-500" : "bg-blue-400"}`}
-            onClick={() => movePage({page:pageNum})}>
+            onClick={() => movePage({page:pageNum, keyword})}>
                 {pageNum}
             </div>
             )}
             
             {serverData.next ? <div className="m-2 p-2 w-16 text-center font-bold text-blue-400"
-            onClick={() => movePage({page: serverData.nextPage})}>다음</div> : <></>}
+            onClick={() => movePage({page: serverData.nextPage, keyword})}>다음</div> : <></>}
         </div>
     )
 }
