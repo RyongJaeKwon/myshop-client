a185d825c4cf9bb1f801dfc1d56ee0db0dd50aec
RyongJaeKwon <gyj343@gmail.com>
2024-07-24 15:27:11 +0900

    Feat: SearchComponent 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
-import BaseLayout from "../layout/BaseLayout";
-import { getRecentList } from "../api/itemApi";
-import GridLayout from "./item/GridLayout";
-import ItemLayout from "./item/ItemLayout";
-import SearchComponent from "../components/item/SearchComponent";
+import React, { useEffect, useState } from "react"
+import BaseLayout from "../layout/BaseLayout"
+import { getRecentList } from "../api/itemApi"
+import GridLayout from "./item/GridLayout"
+import ItemLayout from "./item/ItemLayout"
+import SearchComponent from "../components/item/SearchComponent"
 
 const MainPage = () => {
-const [serverData, setServerData] = useState({ recentItems: [] });
+const [serverData, setServerData] = useState({ recentItems: [] })
 const [refresh, setRefresh] = useState(false)
 
 useEffect(() => {
-    fetchRecentItems();
-}, [refresh]);
+    fetchRecentItems()
+}, [refresh])
 
 const fetchRecentItems = async () => {
     try {
-        const data = await getRecentList();
-        setServerData({ recentItems: data });
+        const data = await getRecentList()
+        setServerData({ recentItems: data })
     } catch (error) {
-        console.error("error: ", error);
+        console.error("error: ", error)
     }
 };
 
 const toggleRefresh = () => {
-    setRefresh(!refresh);
+    setRefresh(!refresh)
 };
 
 return (
     <BaseLayout toggleRefresh={toggleRefresh}>
         <div className="mt-3">
-            <SearchComponent />
+            <SearchComponent initKeyword={""}/>
         </div>
         <GridLayout>
             {serverData.recentItems.map(item => (
@@ -41,4 +41,4 @@ return (
     );
 };
 
-export default MainPage;
+export default MainPage
