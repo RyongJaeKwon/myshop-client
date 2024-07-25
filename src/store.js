96e3e4ebd6bd1dc9da1f700e3b8168fc4e9d1f71
RyongJaeKwon <gyj343@gmail.com>
2024-07-26 05:09:47 +0900

    Feat: replySlice 추가

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from "./slices/loginSlice";
 import cartSlice from "./slices/cartSlice";
+import replySlice from "./slices/replySlice";
 
 export default configureStore({
     reducer: {
         "loginSlice" : loginSlice,
-        "cartSlice" : cartSlice
+        "cartSlice" : cartSlice,
+        "replySlice" : replySlice
     }
 })
