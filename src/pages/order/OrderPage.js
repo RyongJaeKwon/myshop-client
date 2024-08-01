3d3b7e4cba8546108e89a5a88614046eb9ed7cd7
RyongJaeKwon <gyj343@gmail.com>
2024-08-01 16:48:58 +0900

    Feat: 주문 페이지 구현

---
+++ src/pages/order/OrderPage.js
@@ -0,0 +1,13 @@
+import OrderComponent from "../../components/order/OrderComponent"
+
+const OrderPage = () => {
+    return(
+        <div className="w-full h-full">
+            <div className="flex justify-center items-center mt-10">
+                <OrderComponent/>
+            </div>
+        </div>
+    )
+}
+
+export default OrderPage
