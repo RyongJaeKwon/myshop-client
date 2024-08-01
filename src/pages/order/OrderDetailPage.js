91298eb318a8cbfb9a306d7a3eb66abb354554d7
RyongJaeKwon <gyj343@gmail.com>
2024-08-02 02:42:58 +0900

    Feat: 주문 상세 페이지 구현

---
+++ src/pages/order/OrderDetailPage.js
@@ -0,0 +1,13 @@
+import OrderDetailComponent from "../../components/order/OrderDetailComponent"
+
+const OrderDetailPage = () => {
+    return(
+        <div className="w-full h-full">
+            <div className="flex justify-center items-center mt-10">
+                <OrderDetailComponent/>
+            </div>
+        </div>
+    )
+}
+
+export default OrderDetailPage
