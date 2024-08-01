bfe8b7fc08f29667f9932273a021549ec30f4d00
RyongJaeKwon <gyj343@gmail.com>
2024-08-01 16:49:10 +0900

    Feat: 주문목록 페이지 구현

---
+++ src/pages/order/OrderListPage.js
@@ -0,0 +1,13 @@
+import OrderListComponent from "../../components/order/OrderListComponent"
+
+const OrderListPage = () => {
+    return(
+        <div className="w-full h-full">
+            <div className="flex justify-center items-center mt-10">
+                <OrderListComponent/>
+            </div>
+        </div>
+    )
+}
+
+export default OrderListPage
