438b585cebf2ef1e0cfd22dc9de9175f35058c17
RyongJaeKwon <gyj343@gmail.com>
2024-08-01 16:51:47 +0900

    Feat: orderSlice 추가

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from "./slices/loginSlice";
 import cartSlice from "./slices/cartSlice";
 import replySlice from "./slices/replySlice";
+import orderSlice from "./slices/orderSlice";
 
 export default configureStore({
     reducer: {
         "loginSlice" : loginSlice,
         "cartSlice" : cartSlice,
-        "replySlice" : replySlice
+        "replySlice" : replySlice,
+        "orderSlice" : orderSlice
     }
 })
